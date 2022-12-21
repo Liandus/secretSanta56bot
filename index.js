d2533cfcb22a7d0e5828c6eb28cc9adb72fef013
Дмитрий Сосипатров <sosipatrovdmitry@MacBook-Pro-Dmitrij.local>
2022-12-21 03:01:41 +0300

    Работа с бд

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
+const sequelize = require('./db');
+const userModel = require('./models');
+
 const TelegramApi = require('node-telegram-bot-api');
 const TOKEN = '5876855739:AAHX3LHy1aeOKkkl-FNX6dhlC98BsXF9Jd0';
 
-const players =  [
-    // {
-    //     userId: 93753787,
-    //     chatId: -896503131,
-    //     userName: 'tvistept',
-    //     recipientId: 12242525,
-    //     recipientName: 'pizda',
-    // }
-]
 
 const bot = new TelegramApi(TOKEN, {polling: true});
 
@@ -47,7 +41,14 @@ const gameContinueOptions = {
     )
 }
 
-bot.setMyCommands(
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+    } catch (error) {
+        console.log('ошибка подключения к базе');
+    }
+    bot.setMyCommands(
     [
         {
             command: '/start',
@@ -66,23 +67,25 @@ bot.on('message',async (msg) => {
     const text = msg.text;
     const chatId = msg.chat.id;
     const type = msg.chat.type;
-    const user = msg.from.username; 
+    const userName = msg.from.username; 
     const userId = msg.from.id; 
 
     if(text && text.includes('start') && type === 'group') {
         try {
-            await bot.sendMessage(userId, `Привет ${user}! Жди адресата!`);
+            await bot.sendMessage(userId, `Привет ${userName}! Жди адресата!`);
         } catch (error) {
             await bot.sendMessage(chatId, `Ты собираешься стать тайным сантой 56, для начала подружись с ботом!`, requestOptions);
         }
     }
 
     if(text && text.includes('start') && type === 'private') {
-        if(players.find(player => player.userId === userId)) {
-            await bot.sendMessage(chatId, `${user}, ты в игре! Жди 25 декабря, я пришлю тебе адресата.`, gameContinueOptions);
+        const player = await userModel.findOne({where: {userId: userId}})
+        console.log(player);
+        if(player) {
+            await bot.sendMessage(chatId, `${userName}, ты в игре! Жди 25 декабря, я пришлю тебе адресата.`, gameContinueOptions);
         } else {
             await bot.sendMessage(chatId, 
-                `Привет ${user}! Ты собираешься стать тайным сантой! Нажми "Стать Сантой", чтобы записать себя в список участников игры. Ты можешь посмотреть своего адресата, когда список заполнится. Заявки принимаются до 25 декабря. Если передумал, можешь выйти из игры. 25 числа ты узнаешь своего адресата.`,
+                `Привет ${userName}! Ты собираешься стать тайным сантой! Нажми "Стать Сантой", чтобы записать себя в список участников игры. Ты можешь посмотреть своего адресата, когда список заполнится. Заявки принимаются до 25 декабря. Если передумал, можешь выйти из игры. 25 числа ты узнаешь своего адресата.`,
                 gameStartOptions
                 );
         }
@@ -95,26 +98,25 @@ bot.on('callback_query', async msg => {
     const chatId = msg.message.chat.id;
     const userName = msg.from.username; 
     const userId = msg.from.id;
+    const player = await userModel.findOne({where: {userId}})
+    console.log(player);
     if (msg.data === 'go') {
-        if(players.find(player => player.userId === userId)) {
+        if(player) {
             await bot.sendMessage(userId, `${userName}, ты уже записался!`);
     
         } else {
-            players.push(
-                {
-                    userId,
-                    chatId,
-                    userName,
-                }
-            )
+            await userModel.create({
+                            userId,
+                            chatId,
+                            userName,
+                            })
         }
         await bot.sendMessage(chatId, `${userName}, ты в игре! Жди 25 декабря, я пришлю тебе адресата.`, gameContinueOptions);
     }
 
     if (msg.data === 'recipient') {
-        const player = players.find(player => player.userId === userId);
         if(player) {
-            if(player?.recipientId) {
+            if(player.recipientId) {
                 await bot.sendMessage(userId, `${userName}, твой адресат ${player.recipientName}!`);
             } else  {
                 await bot.sendMessage(userId, `${userName}, адресата еще нет!`);
@@ -125,11 +127,12 @@ bot.on('callback_query', async msg => {
     }
 
     if (msg.data === 'out') {
-        const index = players.findIndex(player => {
-            player.userId === userId
-        })
-        players.splice(index, 1)
+        userModel.destroy({where: {userId}})
         return bot.sendMessage(userId, `${userName}, очень жаль!`);
     }
 })
+
+}
+
+start();
 
---
+++ models.js
@@ -0,0 +1,12 @@
+const sequelize = require('./db');
+const {DataTypes} = require('sequelize')
+
+const user = sequelize.define('user', {
+        id: {type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true,},
+        userId: {type: DataTypes.INTEGER, unique: true,},
+        chatId: {type: DataTypes.INTEGER,},
+        userName: {type: DataTypes.STRING,},
+        recipientId: {type: DataTypes.INTEGER, unique: true,},
+        recipientName: {type: DataTypes.STRING, defaultValue: null},
+    });
+module.exports = user
